6ed1ea188b362ed38182dcfe7ba862afcd5238f0
mrzeppa <mrzeppa16@gmail.com>
2020-03-02 14:45:36 +0100

    added method to get verse in range

--- src/test/SongTest.java
+++ src/test/SongTest.java
@@ -75,7 +75,6 @@ public class SongTest {
                 "On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
                 "On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.";
         assertEquals(result, Song.getVerseRange(0, 11));
-
     }
 
 }
