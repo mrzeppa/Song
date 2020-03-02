0985faa809a429be9f8dd7a26570efa8ade172fe
mrzeppa <mrzeppa16@gmail.com>
2020-03-02 14:08:00 +0100

    added test case for getVerse Method

--- src/test/SongTest.java
+++ src/test/SongTest.java
@@ -12,4 +12,9 @@ public class SongTest {
         assertEquals("On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.", Song.getVerse(1));
     }
 
+    @Test
+    public void methodWithGiven_2ShouldReturnSecondVerse(){
+        assertEquals("On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.", Song.getVerse(2));
+    }
+
 }
